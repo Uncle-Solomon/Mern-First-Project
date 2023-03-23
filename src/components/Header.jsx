5ea07fbc09843f349e04a9a8f1c7053ae0286040
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-23 10:40:35 +0100

    Hero Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
   };
   return (
     <div>
-      <section className="sticky top-0 left-0 right-0 z-50">
+      <section className="sticky top-0 left-0 right-0 z-50 bg-white">
         <header className="container mx-auto px-5 py-5 flex justify-between items-center">
           <div>
             <img src={images.Logo} alt="logo" className="w-16" />
