ade707721115ea77393cb30cee526e63fb8abc37
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-24 13:41:59 +0100

    Done with frontend

---
+++ src/components/SocialShareButtons.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaRedditSquare,
+  FaTwitterSquare,
+  FaWhatsappSquare,
+} from "react-icons/fa";
+const SocialShareButtons = ({ url, title }) => {
+  return (
+    <div className="w-full flex justify-between">
+      <a target="_blank" rel="noreferrer" href="/">
+        <FaFacebookSquare className="text-[#3b5998] w-12 h-auto" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://twitter.com/intent/tweet?url=${url}`}
+      >
+        <FaTwitterSquare className="text-[#00acee] w-12 h-auto" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://www.reddit.com/submit?url=${url}&title=${title}`}
+      >
+        <FaRedditSquare className="text-[#ff4500] w-12 h-auto" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://api.whatsapp.com/send/?text=${url}`}
+      >
+        <FaWhatsappSquare className="text-[#25d366] w-12 h-auto" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialShareButtons;
--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import BreadCrumbs from "../../components/BreadCrumbs";
 import CommentsContainer from "../../components/comments/CommentsContainer";
 import MainLayout from "../../components/MainLayout";
+import SocialShareButtons from "../../components/SocialShareButtons";
 
 import { images } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
@@ -107,12 +108,23 @@ const ArticleDetailPage = () => {
           </div>
           <CommentsContainer className="mt-10" loggedInUserId="a" />
         </article>
-        <SuggestedPosts
-          header="Latest Article"
-          posts={postsData}
-          tags={tagsData}
-          className="mt-8 lg:mt-0 lg:max-w-xs"
-        />
+        <div>
+          <SuggestedPosts
+            header="Latest Article"
+            posts={postsData}
+            tags={tagsData}
+            className="mt-8 lg:mt-0 lg:max-w-xs"
+          />
+          <div className="mt-7">
+            <h2 className="font-roboto font-medium text-dak-hard mb-4 md:text-xl">
+              Share On:
+            </h2>
+            <SocialShareButtons
+              url={encodeURI("https://www.instagram.com/")}
+              title={encodeURIComponent("Instagram")}
+            />
+          </div>
+        </div>
       </section>
     </MainLayout>
   );
